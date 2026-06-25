2c977bef4de7fc1ab72cdff0850b67a56eb3ed0b
roshini <roshinipanarajan@gmail.com>
2026-06-25 14:29:09 +0530

    Added daily assignments

---
+++ daily_assignments/Day1_Assignment.java
@@ -0,0 +1,20 @@
+package daily_assignments;
+
+public class Day1_Assignment {
+	
+	public static void main(String[] args) {
+		
+		int studentid = 101;
+		System.out.println(studentid);
+		
+	    char grade ='A';
+		System.out.println(grade);
+		
+		boolean result=true;
+		System.out.println(result);
+		
+		short age = 18;
+		System.out.println(age);
+	}
+
+}
---
+++ daily_assignments/Day2_Assignment.java
@@ -0,0 +1,43 @@
+package daily_assignments;
+
+public class Day2_Assignment {
+	
+	public static void main(String[] args) {
+		
+		// Assignment 1
+		char alpha = 'A';
+		int value = alpha;
+		System.out.println("ASCII CODE :" +value);	
+		
+		char alpha1 = 'a';
+		int value1 = alpha1;
+		System.out.println("ASCII CODE : " +value1);
+		
+		char alpha2 = '0';
+		int value2 = alpha2;
+		System.out.println("ASCII CODE : " +value2);
+		
+		char alpha3 = '@';
+		int value3 = alpha3;
+		System.out.println("ASCIT CODE : " +value3);
+		
+		
+		//Assignment 2
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		
+		
+		//Assignment 3
+		double num = 10.75;
+        int intNum = (int) num;
+
+        System.out.println("Double value = " + num);
+        System.out.println("Integer value after type casting = " + intNum);
+		
+		
+	}
+
+}
