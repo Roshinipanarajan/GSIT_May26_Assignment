5e0a55c2da04e76dd455e54d61dd33b47e0b4a1a
roshini <roshinipanarajan@gmail.com>
2026-06-25 22:53:13 +0530

    Added Day3 Assignment

---
+++ daily_assignments/Day3_Assignment.java
@@ -0,0 +1,26 @@
+package daily_assignments;
+
+public class Day3_Assignment {
+	
+	public static void main(String[] args) {
+		
+		int mark = 95;
+		
+		if(mark>=90) {
+			System.out.println("You got \"A\" Grade");
+		}
+		else if(mark>=75) {
+			System.out.println("You got \"B\" Grade");
+		}
+		else if(mark>=60) {
+			System.out.println("You got \"C\" Grade");
+		}
+		else if(mark>=40) {
+			System.out.println("You got \"D\" Grade");
+		}
+		else{
+			System.out.println("Fail");
+		}
+	}
+
+}
